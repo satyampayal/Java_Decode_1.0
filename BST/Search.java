89b87e8f789afd72423783e271a83f452222b81f
satyam <satyampayal10@gmail.com>
2024-07-10 12:44:58 +0530

    Binary search Tree

---
+++ BST/Search.java
@@ -0,0 +1,24 @@
+package BST;
+
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val=val;
+    }
+}
+
+public class Search {
+    //   public TreeNode searchBST(TreeNode root, int val) {
+        
+    //     if(root==null) return null;
+    //     if(val<root.val) return searchBST(root.left,val);
+    //     else if(val>root.val) return searchBST(root.right,val);
+    //     else return root;
+  //  }
+    public static void main(String[] args) {
+        
+    }
+}
