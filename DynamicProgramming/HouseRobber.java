7f064d4565061198e985d7aacf37788e31f6bd26
satyam <satyampayal10@gmail.com>
2024-08-15 20:42:13 +0530

    House Ropper Tabulation

--- DynamicProgramming/HouseRobber.java
+++ DynamicProgramming/HouseRobber.java
@@ -1,25 +1,22 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class HouseRobber {
-    public static int rob(int[] arr) {
-        int maxAmount=-1;
-        int n=arr.length;
-        for(int i=0;i<n-2;i++){
-            int k=i+2;
-            int temp=k;
-            int sumAmount=0;
-            for(int j=0;j<n-i-2;j++){
-                while(k<n){
-                     sumAmount+=arr[k];
-                     k+=2;
-                }
-                maxAmount=Math.max(sumAmount,maxAmount);
-                temp=temp+1;
-                k=temp;
-            }
-        }
-        return maxAmount;
+    // recursion + memoziation
+           public static int amount(int[] nums,int i,int[] dp) {
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1 ) return dp[i];
+        int take=nums[i]+ amount(nums,i+2,dp);
         
+        int skip=amount(nums,i+1,dp);
+        return  dp[i]=Math.max(take,skip);
+        
+    }
+    public static int rob(int[] nums) {
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+         return amount(nums,0,dp);
     }
 
     public static void main(String[] args) {
--- DynamicProgramming/HouseRobberTabulation.java
+++ DynamicProgramming/HouseRobberTabulation.java
@@ -1,5 +1,26 @@
 package DynamicProgramming;
 
+
 public class HouseRobberTabulation {
     
+
+public static int rob(int[] nums) {
+    int n=nums.length;
+    if(n==1) return nums[0];
+    int[] dp=new int[n];
+  dp[0]=nums[0];
+  dp[1]=Math.max(nums[0],nums[1]);
+    for(int i=2;i<n;i++){
+       dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+
+    }
+    return dp[n-1];
+    
+}
+
+public static void main(String[] args) {
+    int[] arr={5,15,6,20,22};
+    System.out.println(rob(arr));
+}
+
 }
