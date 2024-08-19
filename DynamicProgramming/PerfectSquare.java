f55247a3c0054cfea4f472c4f982e3c036a41fbe
satyam <satyampayal10@gmail.com>
2024-08-19 22:04:06 +0530

    Perfect Squre Recurssion

--- DynamicProgramming/PerfectSquare.java
+++ DynamicProgramming/PerfectSquare.java
@@ -1,21 +1,30 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PerfectSquare {
     public static boolean isPrefectSqure(int n){
         int s=(int)(Math.sqrt(n));
         return s*s==n;
     }
-    public static int numSquare(int n){
+    public static int numSquare(int n, int[] dp){
         if(isPrefectSqure(n)) return 1;
+        if(dp[n]!=-1) return dp[n];
         int min=Integer.MAX_VALUE;
         for(int i=1;i<=n/2;i++){
-            int count=numSquare(i)+numSquare(n-i);
+            int count=numSquare(i,dp)+numSquare(n-i,dp);
             min=Math.min(min,count);
         }
-        return min;
+        return  dp[n]= min;
 
     }
     public static void main(String[] args) {
-        System.out.println(numSquare(13));
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(numSquare(n,dp));
+        sc.close();
     }
 }
